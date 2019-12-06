c55dfdb60dc990b261449b251bca3eace6f16dc7
ajmerr13 <oliverclosoff07@gmail.com>
2019-12-06 12:49:04 -0500

    mvp

---
+++ client/src/components/_tests_/App.test.js
@@ -0,0 +1,30 @@
+import * as rtl from '@testing-library/react';
+import '@testing-library/jest-dom/extend-expect';
+import App from './App';
+import PlayerCard from './components/PlayerCard';
+
+afterEach(rtl.cleanup);
+
+it("The App renders without crashing", () => {
+  rtl.render(<App />);
+});
+
+
+it("Players are defined", () => {
+  const players = <PlayerCard />;
+  expect(players).toBeDefined();
+  expect(players).not.toBeUndefined();
+  expect(players).not.toBeFalsy();
+});
+
+it("Renders Womens World Cup h1", () => {
+  const wrapper = rtl.render(<App />);
+  expect(wrapper.getByText(/women's world cup/i));
+});
+
+test("useDarkMode is defined", () => {
+  const darkmode = <useDarkMode />;
+  expect(darkmode).toBeDefined();
+  expect(darkmode).not.toBeUndefined();
+  expect(darkmode).not.toBeFalsy();
+});
